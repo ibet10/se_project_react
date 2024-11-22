2d0160075cc8c64ddf31e37520abf428de86ba2f
ibet1 <contact.ibet10raya@gmail.com>
2024-11-22 16:28:01 -0500

    S10 - add Footer feature and styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
@@ -11,6 +12,7 @@ function App() {
       <div className="page__content">
         <Header />
         <Main weatherData={weatherData} />
+        <Footer />
       </div>
     </div>
   );
