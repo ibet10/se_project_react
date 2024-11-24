6e7d449bab7cc950a77dd5ce9f7d97917bec721b
ibet1 <contact.ibet10raya@gmail.com>
2024-11-24 15:20:01 -0500

    S10 fix radio buttons, fonts, and page-sizing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__title">Developed by Name Surname</p>
-      <p className="footer__year">2024 PLACEHOLDER</p>
+      <p className="footer__title">Developed by Ibet Raya</p>
+      <p className="footer__year">2024</p>
     </footer>
   );
 }
