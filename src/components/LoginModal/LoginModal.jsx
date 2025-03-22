cd4fb7cf73aad551014faa6e9048d773144f9dc5
ibet1 <contact.ibet10raya@gmail.com>
2025-03-21 22:33:31 -0400

    S14 - add RegisterModal and LoginModal

---
+++ src/components/LoginModal/LoginModal.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+
+const LoginModal = ({ isOpen, onLogin, closeActiveModal, buttonText }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin({ email, password });
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setEmail("");
+      setPassword("");
+    }
+  }, [isOpen]);
+
+  return (
+    <ModalWithForm
+      isOpen={isOpen === "login"}
+      closeActiveModal={closeActiveModal}
+      onSubmit={handleSubmit}
+      title="Log in"
+      buttonText={buttonText}
+    >
+      <label className="modal__label" htmlFor="email-login">
+        Email
+        <input
+          className="modal__input"
+          id="email-login"
+          type="email"
+          placeholder="Email"
+          value={email}
+          minLength="1"
+          maxLength="60"
+          required
+          onChange={handleEmailChange}
+        />
+      </label>
+
+      <label className="modal__label" htmlFor="password-login">
+        Password
+        <input
+          className="modal__input"
+          id="password-login"
+          type="password"
+          placeholder="Password"
+          value={password}
+          minLength="1"
+          maxLength="30"
+          required
+          onChange={handlePasswordChange}
+        />
+      </label>
+    </ModalWithForm>
+
+    //INCOMPLETE -- ADD BUTTONS "Login or Sign up"
+  );
+};
+
+export default LoginModal;
---
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+
+const RegisterModal = ({
+  isOpen,
+  onRegister,
+  closeActiveModal,
+  buttonText,
+}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleAvatarChange = (e) => {
+    setAvatar(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onRegister({ name, avatar, email, password });
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setName("");
+      setAvatar("");
+      setEmail("");
+      setPassword("");
+    }
+  }, [isOpen]);
+
+  return (
+    <ModalWithForm
+      isOpen={isOpen === "register"}
+      closeActiveModal={closeActiveModal}
+      onSubmit={handleSubmit}
+      title="Sign Up"
+      buttonText={buttonText}
+    >
+      <label className="modal__label" htmlFor="email-register">
+        Email*{" "}
+        <input
+          className="modal__input"
+          type="email"
+          id="email-register"
+          placeholder="Email"
+          name="email"
+          value={email}
+          minLength="1"
+          maxLength="60"
+          required
+          onChange={handleEmailChange}
+        />
+      </label>
+
+      <label className="modal__label" htmlFor="password-register">
+        Password*{" "}
+        <input
+          className="modal__input"
+          type="password"
+          id="password-register"
+          placeholder="Password"
+          name="password"
+          value={password}
+          minLength="1"
+          maxLength="30"
+          required
+          onChange={handlePasswordChange}
+        />
+      </label>
+
+      <label className="modal__label" htmlFor="name-register">
+        Name*{" "}
+        <input
+          className="modal__input"
+          type="text"
+          id="name-register"
+          placeholder="Name"
+          name="name"
+          value={name}
+          minLength="1"
+          maxLength="30"
+          required
+          onChange={handleNameChange}
+        />
+      </label>
+
+      <label className="modal__label" htmlFor="avatar-register">
+        Avatar URL*{" "}
+        <input
+          className="modal__input"
+          type="url"
+          id="avatar-register"
+          placeholder="Avatar URL"
+          name="avatar"
+          value={avatar}
+          minLength="1"
+          required
+          onChange={handleAvatarChange}
+        />
+      </label>
+    </ModalWithForm>
+
+    //INCOMPLETE -- ADD BUTTONS "Sign up or Login"
+  );
+};
+
+export default RegisterModal;
